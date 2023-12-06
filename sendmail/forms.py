5cca1f5a26ab32dbcfa36571ccf039188821ff6d
natalya tomova <natalyayurevna19@gmail.com>
2023-12-06 19:55:14 +1000

    Add widgets for DateTimeField

--- sendmail/forms.py
+++ sendmail/forms.py
@@ -10,4 +10,7 @@ class MailingCreateForm(forms.ModelForm):
         model = Mailing
         exclude = ('next_date',)
 
-
+        widgets = {
+            'start_date': TextInput(attrs={'placeholder': 'ДД.ММ.ГГГГ ЧЧ:ММ:СС'}),
+            'end_date': TextInput(attrs={'placeholder': 'ДД.ММ.ГГГГ ЧЧ:ММ:СС'}),
+        }
